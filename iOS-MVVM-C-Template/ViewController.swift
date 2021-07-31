24fe0d1b5b511918e9a259c5b2116a1dcb026455
Renato Mateus <renato.santos@zallpy.com>
2021-07-31 18:52:04 -0300

    Initial Commit

---
+++ iOS-MVVM-C-Template/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-MVVM-C-Template
+//
+//  Created by Renato Mateus on 31/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
